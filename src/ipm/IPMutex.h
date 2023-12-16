7b40a75262194e2f3097e9b4ff1c0d27a964e650
JunYoungJo <junyoungjo@kakao.com>
2023-12-16 15:49:26 +0900

    Initial commit

---
+++ src/ipm/IPMutex.h
@@ -0,0 +1,14 @@
+//
+// Created by junyoung on 2023-12-16.
+//
+
+#ifndef IPMUTEX_IPMUTEX_H
+#define IPMUTEX_IPMUTEX_H
+
+namespace ipm {
+    class IPMutex {
+
+    };
+} // namespace ipm
+
+#endif //IPMUTEX_IPMUTEX_H
